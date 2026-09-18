01a02e9c2f92de81d969d72ae5bf7c4da9a557c3
Ricardo Rocha <dev.ricardorocha@gmail.com>
2026-09-18 14:39:52 -0300

    api base

---
+++ api/src/main/java/com/devricardorocha/fogaspdv_api/FogaspdvApiApplication.java
@@ -0,0 +1,22 @@
+package com.devricardorocha.fogaspdv_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class FogaspdvApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FogaspdvApiApplication.class, args);
+	}
+
+	@GetMapping("/welcome")
+	public ResponseEntity<String> welcome() {
+		return ResponseEntity.ok("Welcome!");
+	}
+
+}
---
+++ api/src/test/java/com/devricardorocha/fogaspdv_api/FogaspdvApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devricardorocha.fogaspdv_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FogaspdvApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
